0e7d093f5bde07298d630300f64675f84d3c5786
Vishwajeet Salunke <salunkevishwajeet577@gmail.com>
2023-02-07 14:16:24 +0000

    updated repo

--- doubly linked list.cpp
+++ doubly linked list.cpp
@@ -2,98 +2,66 @@
 using namespace std;
 class node
 {
-  public:
-  int data;
-  node *next;
-  node *prev;
-  node(int data)
-  {
-    this->data = data;
-    this->next = NULL;
-    this->prev = NULL;
-  }
+    public:
+    int data;
+    node *next;
+    node *prev;
+    node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
 };
-
-node *insertBegin(int data,node *head)
-{
-    node *created = new node(data);
-    created->next = head;
-    head->prev = created;
-    head = created;
-    return head;
-}
-
-node *insertLast(int data, node* head)
+node *insert(int data,node *head)
 {
     node *created = new node(data);
     node *temp = head;
-    if(temp==NULL)
+    if(head==NULL)
     {
-        temp = created;
-        temp->next = NULL;
-        created->prev = NULL;
-        return temp;
-    }
-    while(temp->next!=NULL)
-    {
-        temp=temp->next;
-    }
-    temp->next = created;
-    created->prev = temp;
-    return head;
-}
-
-void traverseLinearly(node *head)
-{
-    node *temp = head;
-    if(temp == NULL)
-    {
-        cout<<"list is empty!!"<<endl;
+        head = created;
+        head->next = head;
+        head->prev = head;
+        return head;
     }
     else
     {
-        while(temp!=NULL)
-        {
-            cout<<temp->data<<" ";
-            temp = temp->next;   
-        }
-        cout<<endl;
-    }
-}
-
-void traverseFromEnd(node *head)
-{
-    node *temp = head;
-    if(temp == NULL)
-    {
-        cout<<"list is empty!!"<<endl;
-    }
-    else
-    {
-        while(temp->next!=NULL)
+        while(temp->next!=head)
         {
             temp = temp->next;
         }
-        while(temp!=NULL)
-        {
-            cout<<temp->data<<" ";
-            temp = temp->prev;
-        }
-        cout<<endl;
-        cout<<endl;
+        created->next = head;
+        created->prev = temp;
+        head->prev = created;
+        temp->next = created;
+
     }
+    return head;
 }
+
+void traverse(node *head)
+{
+    node *temp = head;
+    if(temp==NULL)
+    {
+        cout<<"list is empty"<<endl;
+    }
+    do
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }while(temp!=head);
+    cout<<endl;
+}
+
 int main()
 {
     node *head = NULL;
-    head = insertLast(2,head);
-    head = insertLast(4,head);
-    head = insertLast(6,head);
-    head = insertLast(8,head);
-    head = insertLast(10,head);
-    head = insertLast(12,head);
-    head = insertBegin(70,head);
-    traverseLinearly(head);
-    traverseFromEnd(head);
+    head = insert(2,head);
+    head = insert(4,head);
+    head = insert(6,head);
+    head = insert(8,head);
+    head = insert(10,head);
+    traverse(head);
     return 0;
 }
